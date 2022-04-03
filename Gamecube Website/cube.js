185672a1538fd18f7331ff5c94cf2f5f739b9f1a
Cory98 <coryelliottbyrd@gmail.com>
2022-04-02 21:44:10 -0500

    project update

---
+++ Gamecube Website/cube.js
@@ -0,0 +1,5 @@
+document.getElementById("submit").onclick = function(){
+
+    var myName = document.getElementById("username").value;
+    console.log("Success! your username is",document.getElementById("username").value);
+}
